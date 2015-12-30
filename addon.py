ae3763a374a9da0b5588c7b570b7c8618bd93b76
Damon Toumbourou <dtoumbourou@gmail.com>
2015-12-30 21:40:00 +1100

    ready for repo

--- addon.py
+++ addon.py
@@ -3,6 +3,7 @@ from resources.lib import topdocos
 
 
 PLUGIN_URL = 'plugin://plugin.video.youtube/?action=play_video&videoid='
+SITE_URL = 'http://topdocumentaryfilms.com/'
 
 plugin = Plugin()
 
@@ -11,17 +12,98 @@ plugin = Plugin()
 def main_menu():
 
     items = [
+        {
+            'label': plugin.get_string(30001),
+            'path': plugin.url_for('recent'),
+        },
+        {
+            'label': plugin.get_string(30002),
+            'path': plugin.url_for('highest_rated'),
+        },
+        {
+            'label': plugin.get_string(30003),
+            'path': plugin.url_for('most_voted'),
+        },
+        {
+            'label': plugin.get_string(30004),
+            'path': plugin.url_for('most_shared'),
+        },
         { 
-            'label': plugin.get_string(30000),
-            'path': plugin.url_for('doco_categorys'),
+            'label': plugin.get_string(30005),
+            'path': plugin.url_for('categorys'),
         }
     ]
     
     return items
 
 
-@plugin.route('/doco_categorys/')
-def doco_categorys():
+@plugin.route('/recent/')
+def recent():
+    
+    items = []
+
+    content = topdocos.get_section(SITE_URL, 'recently added')
+
+    for i in content:
+        items.append({
+            'label': i['label'],
+            'path': plugin.url_for('play_content', url=i['path']),
+            'thumbnail': i['thumbnail'],
+        })
+
+    return items
+
+
+@plugin.route('/highest_rated/')
+def highest_rated():
+
+    items = []
+
+    content = topdocos.get_section(SITE_URL, 'highest rated')
+
+    for i in content:
+        items.append({
+            'label': i['label'],
+            'path': plugin.url_for('play_content', url=i['path']),
+        })
+
+    return items
+
+
+@plugin.route('/most_voted/')
+def most_voted():
+
+    items = []
+
+    content = topdocos.get_section(SITE_URL, 'most voted')
+
+    for i in content:
+        items.append({
+            'label': i['label'],
+            'path': plugin.url_for('play_content', url=i['path']),
+        })
+
+    return items
+
+
+@plugin.route('/most_shared/')
+def most_shared():
+
+    items = []
+
+    content = topdocos.get_section(SITE_URL, 'most shared')
+
+    for i in content:
+        items.append({
+            'label': i['label'],
+            'path': plugin.url_for('play_content', url=i['path']),
+        })
+
+    return items
+
+
+@plugin.route('/categorys/')
+def categorys():
     url = 'http://topdocumentaryfilms.com/all/'
 
     items = []
@@ -37,7 +119,7 @@ def doco_categorys():
     return items
 
 
-@plugin.route('/doco_categorys/<url>')
+@plugin.route('/categorys/<url>')
 def get_categorys(url):
 
     items = []
@@ -47,14 +129,15 @@ def get_categorys(url):
     for i in content:
         items.append({
             'label': i['label'],
-            'path': plugin.url_for('play_categorys', url=i['path']),
+            'path': plugin.url_for('play_content', url=i['path']),
+            'thumbnail': i['thumbnail'],
         })
 
     return items
 
 
 @plugin.route('/doco_categorys/get_categorys/<url>/')
-def play_categorys(url):
+def play_content(url):
 
     items = []
 
--- resources/lib/topdocos.py
+++ resources/lib/topdocos.py
@@ -3,9 +3,15 @@ import requests
 import re
 
 
-def get_categorys(url):
+def get_soup(url):
     page = requests.get(url)
     soup = bs(page.text, 'html.parser')
+    
+    return soup
+
+
+def get_categorys(url):
+    soup = get_soup(url)
     content = soup.find('nav', {'class': 'module clear'})
     content = content.find_all('a')
     
@@ -27,8 +33,7 @@ def get_categorys(url):
 
 
 def get_categorys_content(url):
-    page = requests.get(url)
-    soup = bs(page.text, 'html.parser')
+    soup = get_soup(url)
     content = soup.find_all('article', {'class': 'module'})
 
     output = []
@@ -36,13 +41,16 @@ def get_categorys_content(url):
     for i in content:
         label = i.find('h2')
         label = label.find('a').get('title')
-        
+
         path = i.find('h2')
         path = path.find('a').get('href')
+        
+        thumb = i.find('img').get('src')
 
         items = {
                 'label': label,
                 'path': path,
+                'thumbnail': thumb,
         }
 
         output.append(items)
@@ -51,27 +59,59 @@ def get_categorys_content(url):
 
 
 def play_categorys(url):
-    page = requests.get(url)
-    soup = bs(page.text, 'html.parser')
-    content = soup.find('meta', {'itemprop': 'embedUrl'})
-    
-    output = []
-   
-    path = content.get('content').split('embed/')[1]
+    soup = get_soup(url)
 
-    label = soup.find('meta', {'itemprop': 'name'})
-    label = label.get('content')
-    print 'labe: '
-    print label
+    try:
+        content = soup.find('meta', {'itemprop': 'embedUrl'})
     
-    items = {
-            'label': label,
-            'path': path,
-    }
-
+        output = []
+    
+        path = content.get('content').split('embed/')[1]
+        
+        label = soup.find('meta', {'itemprop': 'name'})
+        label = label.get('content')
+    
+        items = {
+                'label': label,
+                'path': path,
+    
+    except AttributeError:
+        items = {
+                'label': 'Sorry content not avaliable.',
+                'path': ' ',
+        }
+        
     output.append(items)
-
-    return output
     
+    return output   
 
-play_categorys('http://topdocumentaryfilms.com/911-decade-deception/')
+
+def get_section(url, section):
+    soup = get_soup(url)
+    content = soup.find_all('section', {'class': 'module'})
+   
+    output = []
+
+    for i in content:
+        current_section = i.find('h3').get_text().lower()
+        
+        if section in current_section:  
+            content = i.find_all('li')
+            
+            for i in content:
+                label_path = i.find('a')
+                label = label_path.get('title')
+
+                path = label_path.get('href')
+                
+                thumbnail = i.find('img').get('src')
+
+                items = {
+                    'label': label,
+                    'path': path,
+                    'thumbnail': thumbnail,
+                }
+
+                output.append(items)
+    
+    return output
